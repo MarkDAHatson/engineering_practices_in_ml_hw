c24096fd43d7e7cdd6e57af92fb9318098563f4f
MarkHatson <markdanielhatson@gmail.com>
2022-11-15 16:49:31 +0300

    ..

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,14 @@
+import matplotlib.pyplot as plt
 def is_null(train, test):
     print(train.isnull().sum())
     print('------------------')
     print(test.isnull().sum())
+
+def missing_values(train, test):
+    fig = plt.figure(figsize=(8, 5))
+    ax = fig.add_subplot(111)
+    ax = train.boxplot(column='Fare', by=['Embarked', 'Pclass'], ax=ax)
+    plt.axhline(y=80, color='green')
+    ax.set_title('', y=1.1)
+    train[train.Embarked.isnull()][['Fare', 'Pclass', 'Embarked']]
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from prep import create_data, create_data_test, head_tail, distrib_viz, class_age_viz
-from data_cleaning import is_null
+from data_cleaning import is_null, missing_values
 
 train = create_data()
 test = create_data_test()
@@ -9,6 +9,8 @@ head_tail(train, test)
 # class_age_viz(train, test)
 
 is_null(train, test)
+
+missing_values(train, test)
 
 
 
